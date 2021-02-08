dcc4a10a97867d21a762f0bee4b809f749afc39b
Sam Slotemaker <sam.slotemaker@ziggo.nl>
2021-02-08 14:53:39 +0100

    fix backbutton

--- src/js/modules/router.js
+++ src/js/modules/router.js
@@ -2,6 +2,6 @@ import { overview, detail } from './render.js'
 
 export function router() {
     routie('', overview)
-    routie('#overview', overview)
+    routie('overview', overview)
     routie('game/:id', detail)
 }
