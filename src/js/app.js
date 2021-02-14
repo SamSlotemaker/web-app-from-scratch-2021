d26bd1d9c92a3f8771af010c92ad755fe2f9e3f4
Sam Slotemaker <sam.slotemaker@ziggo.nl>
2021-02-14 13:48:40 +0100

    added comments

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,15 @@
 import { overview } from './modules/render/overview.js'
 import { router } from './modules/router/router.js'
-//calls router that will start the app
 
+
+//get chosen filter
 let chosenFilter = JSON.parse(localStorage.getItem('FILTER'))
 
+//if the user has filtered before
 if (chosenFilter) {
     overview(chosenFilter)
 }
+//first time loading page
 else {
     overview('all')
 }
