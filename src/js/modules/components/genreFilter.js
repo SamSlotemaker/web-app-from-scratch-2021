f7ed1d19f84d17a693ef060139d1d4a75a8dc0d4
Sam Slotemaker <sam.slotemaker@ziggo.nl>
2021-02-15 10:43:57 +0100

    overview container is a component now

--- src/js/modules/components/genreFilter.js
+++ src/js/modules/components/genreFilter.js
@@ -2,16 +2,14 @@ import { getGenres } from "../api/getGenres.js";
 const subject = 'games'
 const query = '?page_size=10'
 
-//create a form width genre radios
+//returns HTML of a form width genre radios
 export async function genreForm() {
     const genres = await getGenres(subject, query)
     let genreRadioElements = ''
-
     //get chosenFilter
     let chosenFilter = JSON.parse(localStorage.getItem('FILTER'))
 
     genres.forEach(genre => {
-
         //check if the radio needs to be checked when a certain filter is chosen
         if (chosenFilter === genre) {
             genreRadioElements += `<input name='genres' value="${genre}" id="${genre}" type="radio" checked/>
@@ -23,9 +21,9 @@ export async function genreForm() {
             <label for="${genre}">${genre}</label>
             `
         }
-
     })
 
+    //check of filter has been set to ALL and check radio if so
     let allRadio;
     if (chosenFilter === 'all') {
         allRadio = `<input name='genres' value="all" id="all" type="radio" checked/>`
@@ -34,6 +32,7 @@ export async function genreForm() {
         allRadio = `<input name='genres' value="all" id="all" type="radio"/>`
     }
 
+    //combine form
     let form = `
     <form>
     ${allRadio}
--- src/js/modules/render/overview.js
+++ src/js/modules/render/overview.js
@@ -7,29 +7,33 @@ const mainContainer = document.querySelector('main')
 //create overview page
 export async function overview(genre) {
     clearElement(mainContainer)
-    const overviewContainer =
-        `<h1>Most popular games</h1>
-    <section class="games-section">
-            <h2>Games</h2>
-        <div class="games">
-            <article class="loading"></article>
-            <article class="loading"></article>
-            <article class="loading"></article>
-            <article class="loading"></article>
-            <article class="loading"></article>
-            <article class="loading"></article>
-        </div>
-    </section>
-    <section>
-            <h2>All platforms</h2>
-        <div class="platforms"> </div>
-    </section>`
-
-    mainContainer.insertAdjacentHTML('beforeend', overviewContainer)
+    renderOverviewContainer();
     renderGenreForm()
     renderGameSection(genre);
     renderPlatformSection();
 
+}
+
+function renderOverviewContainer() {
+    const overviewContainer =
+        `<h1>Most popular games</h1>
+<section class="games-section">
+        <h2>Games</h2>
+    <div class="games">
+        <article class="loading"></article>
+        <article class="loading"></article>
+        <article class="loading"></article>
+        <article class="loading"></article>
+        <article class="loading"></article>
+        <article class="loading"></article>
+    </div>
+</section>
+<section>
+        <h2>All platforms</h2>
+    <div class="platforms"> </div>
+</section>`
+
+    mainContainer.insertAdjacentHTML('beforeend', overviewContainer)
 }
 
 async function renderPlatformSection() {
@@ -43,7 +47,7 @@ async function renderPlatformSection() {
 
 async function renderGameSection(genre) {
     const gamesContainer = document.querySelector('.games')
-    clearElement(gamesContainer)
+    clearElement(gamesContainer) //clear huidige lijst met games
 
     const gameList = await createGameList(genre)
     //remove loading state when data is loaded
@@ -57,7 +61,6 @@ async function renderGameSection(genre) {
 }
 async function renderGenreForm() {
     const gamesSection = document.querySelector('.games-section')
-
     // add filter to html 
     const genreFormHTML = await genreForm()
     gamesSection.insertAdjacentHTML('afterbegin', genreFormHTML)
